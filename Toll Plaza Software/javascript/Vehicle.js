b36cf862be37b6ee6bc3f4c44b24b08b55105f4a
C R Reshma <55027348+reshma-cr@users.noreply.github.com>
2023-09-08 16:52:59 +0530

    Add files via upload

---
+++ Toll Plaza Software/javascript/Vehicle.js
@@ -0,0 +1,10 @@
+
+
+class Vehicle {
+    constructor(rno, category, date, amount) {
+        this.rno = rno;
+        this.category = category;
+        this.date = date;
+        this.amount = amount;
+    }
+}
